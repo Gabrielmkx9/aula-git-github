6745d8b3b851a4dbf7348355dae8d5dcbfcbda92
Gabrielmkx9 <gabrielwwe557@gmail.com>
2025-08-28 20:38:35 -0300

    Update App.java 2

--- teste/src/App.java
+++ teste/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("FEijoada");
+    package teste;
+public class Teste {
+public static void main(String[] args) {
+int num1 = 3, num2 = 5, soma;
+soma = num1 + num2;
+System.out.println(soma);
+}
+}
     }
 }
